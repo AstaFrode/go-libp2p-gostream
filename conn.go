8050802a5d8b6ac74dd70a715859f58f3a4e0fa4
AstaFrode <frode@cess.one>
2023-11-09 17:53:51 +0800

    rename pkg anem

--- conn.go
+++ conn.go
@@ -4,10 +4,10 @@ import (
 	"context"
 	"net"
 
-	"github.com/libp2p/go-libp2p/core/host"
-	"github.com/libp2p/go-libp2p/core/network"
-	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/libp2p/go-libp2p/core/protocol"
+	"github.com/AstaFrode/go-libp2p/core/host"
+	"github.com/AstaFrode/go-libp2p/core/network"
+	"github.com/AstaFrode/go-libp2p/core/peer"
+	"github.com/AstaFrode/go-libp2p/core/protocol"
 )
 
 // conn is an implementation of net.Conn which wraps
